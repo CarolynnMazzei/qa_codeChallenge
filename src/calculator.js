5eb4e8a5b3d1b2ed6ff7ec5911f215ac381e4fad
Carolynn <carolynnmazzei@gmail.com>
2021-04-25 22:21:30 -0400

    getting my project ready for submission

---
+++ src/calculator.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+/** calculator has simple calculation methods */
+var calculator = {
+    /** Will add x and y together */
+    add: function (x, y) { return x + y; },
+    /** will subtract y from x */
+    subtract: function (x, y) { return x - y; },
+    /** will multiply x and y */
+    multiply: function (x, y) { return x * y; },
+    /** will divide x by y */
+    divide: function (x, y) { return x / y; }
+};
+exports["default"] = calculator;
